c227e5ff86f141ec75170250beb096fcf71200d6
Rodrigo Balmaceda <balmarodrigo@hotmail.com>
2020-12-20 18:42:44 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const port = process.env.PORT || 3977;
+const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
+
+// Para evitar error que sale por consola
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+mongoose.connect(
+  `mongodb://${IP_SERVER}:${PORT_DB}/glutencero`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, res) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("La conexion a la base de datos es correcta.");
+
+      app.listen(port, () => {
+        console.log("######################");
+        console.log("###### API REST ######");
+        console.log("######################");
+        console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
+      });
+    }
+  }
+);
