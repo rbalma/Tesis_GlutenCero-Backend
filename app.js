ba630f8c6a061107e334f408c2e6f4a8610280f1
Rodrigo Balmaceda <balmarodrigo@hotmail.com>
2021-01-24 13:19:45 -0300

    Corregido Login

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const app = express();
+
 const { API_VERSION } = require("./config");
 
 // Load routing
@@ -10,6 +12,7 @@ const userRoutes = require("./routers/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 // Configure Header HTTP
 app.use((req, res, next) => {
